c2cb9bdfab83db5dbef00be6ac6285e7cb04b0a5
Sobolev Aleksandr <sob-al@mail.ru>
2021-01-17 18:02:10 +0500

    js finish

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	// Custom JS
+	const burger = document.querySelector('#burger-menu')
+	const headerNav = document.querySelector('#header-nav')
+
+	const searchBtn = document.querySelector('#search-btn')
+	const searchInput = document.querySelector('#site-search')
+
+	function menuShow() {
+		headerNav.classList.toggle('active')
+	}
+	burger.addEventListener('click', menuShow)
+
+	function searchInputShow(event) {
+		event.preventDefault()
+		searchInput.classList.toggle('showInput')
+	}
+	searchBtn.addEventListener('click', searchInputShow)
 
 });
